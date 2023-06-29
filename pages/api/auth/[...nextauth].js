ddbd85a3764b962f51e28e14ab282cbfd3fc7587
rng190001 <rng190001@utdallas.edu>
2023-06-28 22:24:34 -0500

    Added Components to Authorization

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,28 @@
 import NextAuth from "next-auth"
 import SpotifyProvider from "next-auth/providers/spotify"
-import LOGIN_URL from "../../../lib/spotify"
+import spotifyApi, {LOGIN_URL} from "../../../lib/spotify";
+
+async function refreshAccessToken(token){
+    try {
+      spotifyApi.setAccessToken(token.accessToken);
+      spotifyApi.setRefreshToken(tokn.refreshAccessToken);
+
+      const{refreshedToken} = await spotifyApi.refreshAccessToken();
+
+      return{
+        ...token,
+        accessToken: refreshedToken.access_token,
+        accessTokenExpires: Date.now + refreshedToken.expires_in * 1000,
+        refreshToken: refreshedToken.refresh_token ?? token.refreshToken,
+      }
+    } catch (error) {
+      console.log(error);
+      return{
+        ...token,
+        error: 'Refresh Access Token Error',
+      }
+    }
+}
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -15,7 +37,35 @@ export default NextAuth({
   secret: process.env.JWT_SECRET,
   pages:{
       signIn: '/login'
-  }
-});
+  },
+  callbacks:{
+    async jwt({token, account, user}){
+      //initial sign in 
+      if (account && user){
+        return{
+          ...token,
+          accessToken: account.access_token,
+          refreshToken: account.refresh_token,
+          username: account.providerAccountId,
+          accessTokenExpires: account.expires_at * 1000,
 
-export default NextAuth(authOptions)
+        }
+      }
+
+      //not expired
+      if (Date.now < token.accessTokenExpires){
+        return token;
+      }
+
+      //expired token, need to refresh
+      return await refreshAccessToken(token);
+    },
+    async session({session, token}){
+      session.user.accessToken = token.accessToken;
+      session.user.refreshToken = token.refreshToken;
+      session.user.username = token.username;
+
+      return session;
+    }
+  },
+});
