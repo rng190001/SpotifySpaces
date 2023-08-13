be8801d7243aa5306a3e8bff2d201fb3a308b180
rng190001 <rng190001@utdallas.edu>
2023-08-12 23:33:28 -0500

    Changed the dashboard page

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,6 +1,7 @@
 import { getSession } from 'next-auth/react';
 import { useSession } from "next-auth/react";
 import Sidebar from '@/components/Sidebar';
+import Player from '@/components/Player';
 
 function Dashboard() {
     const {data:session} = useSession();
@@ -9,6 +10,11 @@ function Dashboard() {
       <div className="flex text-white">
       <Sidebar/>
         <h2>Hello from Dashboard!, {session?.user.name}</h2>
+      </div>
+
+      <div className="sticky bottom-0">
+        {/* Player*/}
+        <Player/>
       </div>
     </div>
   )
